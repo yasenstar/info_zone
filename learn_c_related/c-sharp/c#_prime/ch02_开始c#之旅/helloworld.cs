c2970d19beeec20f4c435f13c19c1d172f609b50
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2021-09-21 18:11:58 -0400

    move C-charp related learning in

---
+++ learn_c_related/c-sharp/c#_prime/ch02_开始c#之旅/helloworld.cs
@@ -0,0 +1,9 @@
+using System;
+
+static void Main(string[] args)
+{
+    Console.WriteLine("Hello World! ");
+    Console.ReadLine();
+}
+
+Main(null);
---
+++ learn_c_related/c-sharp/ms_learn_c#/test.cs
@@ -0,0 +1,10 @@
+using System;
+
+static void Main(string[] args)
+{
+    Console.WriteLine("Hello World! ");
+    Console.WriteLine(5 / 10);
+    Console.ReadLine();
+}
+
+Main(null);

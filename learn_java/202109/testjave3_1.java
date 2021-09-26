1346f6f5a62dde1a4a74089587fa57d4ebe63dd5
Xiaoqi Zhao (Mac) <xiaoqizhao@outlook.com>
2021-09-26 09:25:00 -0400

    add testjave3_1.java

---
+++ learn_java/202109/testjave3_1.java
@@ -0,0 +1,14 @@
+/**
+ * @ClassName: TestJava3-1
+ * @Description:这是一个Java简单范例
+ * @Author: Yasen
+ * @date: 2021-09-26
+ */
+public class testjave3_1 {
+    public static void main(String[] args){
+        int num;    //声明一个整型变量num
+        num = 5;
+        System.out.println("这是数字"+num);
+        System.out.println("我有"+num+"本书！");
+    }
+}

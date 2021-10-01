f276047386a4725e04371d34c7c28b42d1ce5629
Xiaoqi Zhao (Mac) <xiaoqizhao@outlook.com>
2021-10-01 19:40:40 -0400

    start learning chapter 08

---
+++ learn_java/beginprogwithjava/ch06-ch08/LowerToUpper.java
@@ -0,0 +1,8 @@
+public class LowerToUpper {
+    public static void main(String args[]){
+        char smallLetter, bigLetter;
+        smallLetter = 'z';
+        bigLetter = Character.toUpperCase(smallLetter);
+        System.out.println("Small Letter: " + smallLetter + ", Big Letter: " + bigLetter);
+    }
+}

23febc9a2c9c21aa831e780103e89e075c741cd2
Xiaoqi Zhao (Mac) <xiaoqizhao@outlook.com>
2021-10-01 19:49:24 -0400

    add ReverseWord.java

--- learn_java/beginprogwithjava/ch06-ch08/LowerToUpper.java
+++ learn_java/beginprogwithjava/ch06-ch08/LowerToUpper.java
@@ -1,7 +1,7 @@
 public class LowerToUpper {
     public static void main(String args[]){
         char smallLetter, bigLetter;
-        smallLetter = 'z';
+        smallLetter = 'e';
         bigLetter = Character.toUpperCase(smallLetter);
         System.out.println("Small Letter: " + smallLetter + ", Big Letter: " + bigLetter);
     }
---
+++ learn_java/beginprogwithjava/ch06-ch08/ReverseWord.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ReverseWord {
+    public static void main(String args[]){
+        Scanner keyboard = new Scanner(System.in);
+
+        char c1, c2, c3, c4;
+
+        c1 = keyboard.findWithinHorizon(".", 0).charAt(0);
+        c2 = keyboard.findWithinHorizon(".", 0).charAt(0);
+        c3 = keyboard.findWithinHorizon(".", 0).charAt(0);
+        c4 = keyboard.findWithinHorizon(".", 0).charAt(0);
+
+        System.out.print(c4);
+        System.out.print(c4);
+        System.out.print(c4);
+        System.out.println(c1);
+
+        keyboard.close();
+    }
+}

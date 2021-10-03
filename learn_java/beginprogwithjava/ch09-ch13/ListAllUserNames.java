2210c8ff11bfa137957f3edeae39d44ff3614dd6
Yasen <xiaoqizhao@outlook.com>
2021-10-03 17:10:29 -0400

    update List All User Names from email addresses file

--- learn_java/beginprogwithjava/ch09-ch13/ListAllUserNames.java
+++ learn_java/beginprogwithjava/ch09-ch13/ListAllUserNames.java
@@ -5,6 +5,22 @@ import java.io.PrintStream;
 
 public class ListAllUserNames {
     public static void main(String args[]) throws FileNotFoundException {
-        
+
+        Scanner diskScanner = new Scanner(new File("email.txt"));
+        PrintStream diskWriter = new PrintStream("usernames.txt");
+        char symbol;
+
+        while (diskScanner.hasNext()) {
+            symbol = diskScanner.findWithinHorizon(".", 0).charAt(0);
+            while (symbol != '@') {
+                diskWriter.print(symbol);
+                symbol = diskScanner.findWithinHorizon(".", 0).charAt(0);
+            }
+            diskScanner.nextLine();
+            diskWriter.println();
+        }
+
+        diskScanner.close();
+        diskWriter.close();
     }
 }

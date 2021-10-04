177394346ea179b985622c183c714c6ecbd02c59
Xiaoqi Zhao (Mac) <xiaoqizhao@outlook.com>
2021-10-04 18:28:38 -0400

    working on ProcessNiceAccounts

--- learn_java/beginprogwithjava/ch17-ch18/ProcessAccounts.java
+++ learn_java/beginprogwithjava/ch17-ch18/ProcessAccounts.java
@@ -9,10 +9,7 @@ class ProcessAccounts {
         for (int i = 0; i < 3; i++) {
             anAccount = new Account();
 
-            anAccount.lastName = "" + 
-                (char) (myRandom.nextInt(26) + 'A') + 
-                (char) (myRandom.nextInt(26) + 'a') + 
-                (char) (myRandom.nextInt(26) + 'a');
+            anAccount.lastName = "" + (char)(myRandom.nextInt(26) + 'A') + (char)(myRandom.nextInt(26) + 'a') + (char)(myRandom.nextInt(26) + 'a');
 
             anAccount.id = myRandom.nextInt(10000);
             anAccount.balance = myRandom.nextInt(10000);

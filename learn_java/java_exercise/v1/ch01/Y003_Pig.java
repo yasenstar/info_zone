8ed74a6d1d8bcfe31584b5d7e1e54bd82bcd3ba7
Yasen <xiaoqizhao@outlook.com>
2021-10-05 05:24:33 -0400

    Y003_Pig picture

---
+++ learn_java/java_exercise/v1/ch01/Y003_Pig.java
@@ -0,0 +1,8 @@
+public class Y003_Pig {
+    public static void main(String args[]) {
+        System.out.println("   (~~~~~~~)  ");
+        System.out.println("  {/ 0   0 /}  ");
+        System.out.println("   (  (00) )   ");
+        System.out.println("   ~   ~~~~   ");
+    }
+}

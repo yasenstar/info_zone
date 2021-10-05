a0fc502b607b5c47fa7c123057e2eded7abb76ad
Yasen <xiaoqizhao@outlook.com>
2021-10-05 11:54:19 -0400

    add Repetition via foreach

---
+++ learn_java/零基础学Java/ch03/Repetition.java
@@ -0,0 +1,13 @@
+// using "foreach" concept
+
+public class Repetition {
+    public static void main(String args[]) {
+        int arr[] = { 7, 10, 1, 3, 6, 798, 34,2};
+
+        for (int x:arr) {
+            System.out.print(x + "\t");
+        }
+
+        System.out.println();
+    }
+}

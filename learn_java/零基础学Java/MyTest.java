83f5038e6425a035bb44a28262e0498b6ebc0af5
Yasen <xiaoqizhao@outlook.com>
2021-10-05 05:38:02 -0400

    add MyTest, try debugging

---
+++ learn_java/零基础学Java/MyTest.java
@@ -0,0 +1,10 @@
+public class MyTest {
+    public static void main(String args[]) {
+
+        System.out.println("first line");
+
+        System.out.println("2nd line");
+
+        System.out.println("3rd line");
+    }
+}

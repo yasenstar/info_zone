4fd13947db2134d6529c3f9cb4a607e851d89f96
Xiaoqi Zhao (Mac) <xiaoqizhao@outlook.com>
2021-10-10 14:06:59 -0400

    try Math_Array (not finished)

---
+++ learn_java/math_array.java
@@ -0,0 +1,7 @@
+public class math_array {
+    public static void main(String args[]) {
+        ArrayList myArray[] = new ArrayList();
+        myArray[0] = 1;
+        System.out.println(myArray[1]);
+    }
+}

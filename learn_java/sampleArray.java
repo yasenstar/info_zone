1cfac0765a97706c70067cc262bea9f8790025c3
Xiaoqi Zhao (Mac) <xiaoqizhao@outlook.com>
2021-10-15 20:55:24 -0400

    learn Math_Array in java

---
+++ learn_java/sampleArray.java
@@ -0,0 +1,8 @@
+public class sampleArray {
+    public static void main(String args[]) {
+        String arr[] = { "Apple", "Banana", "Cherry" };
+        for (String s: arr) {
+            System.out.print()
+        }
+    }
+}

80d432583b0a4d363ed94774336728026f0225e2
Xiaoqi Zhao (Mac) <xiaoqizhao@outlook.com>
2021-10-26 13:33:48 -0400

    add Area Computing sample

--- learn_java/Java语言程序设计3/ComputeArea.java
+++ learn_java/Java语言程序设计3/ComputeArea.java
@@ -1,5 +1,16 @@
-package learn_java.Java语言程序设计3;
+// package learn_java.Java语言程序设计3;
+
+import java.util.Scanner;
 
 public class ComputeArea {
-    
+    public static void main(String args[]){
+        double radius;
+        double area;
+        Scanner input = new Scanner(System.in);
+        System.out.print("请输入半径值： ");
+        radius = input.nextDouble();
+        area = radius * radius * Math.PI;
+        System.out.println("圆的面积是： " + area);
+        input.close();
+    }
 }

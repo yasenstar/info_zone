e1d1e51badfa78ad3df7252f89c97a6c5c521677
Xiaoqi Zhao (Mac) <xiaoqizhao@outlook.com>
2021-11-06 02:55:42 -0400

    multiply table

--- learn_java/Java-beginner/ch01-03/multiplytable.java
+++ learn_java/Java-beginner/ch01-03/multiplytable.java
@@ -4,7 +4,7 @@ public class multiplytable {
         int x, y;
         for (x=1; x<=12; x++) {
             for (y=1; y<=12; y++) {
-                System.out.print(x + " * " + y + " = " + x*y + "\t");
+                System.out.print(x + " * " + y + " = " + (x * y) + "\t");
             }
             System.out.println();
         }

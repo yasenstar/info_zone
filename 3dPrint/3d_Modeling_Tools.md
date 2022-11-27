cf22f89c270eb98a30a408e851958f250b9aa439
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2022-11-27 14:30:14 -0500

    add 3d printing modeling tool

---
+++ 3dPrint/3d_Modeling_Tools.md
@@ -0,0 +1,16 @@
+# 3D Modeling Tools
+
+## TinkerCAD - from AutoDesk
+
+https://www.tinkercad.com/
+
+Directly modeling online.
+
+## 3D Slash - easy free 3D modeling software for beginners
+
+https://www.3dslash.net/
+
+## FreeCAD
+
+https://www.freecadweb.org/
+
--- diagrams/archi_repo.py
+++ diagrams/archi_repo.py
@@ -4,7 +4,7 @@ from diagrams.azure.network import *
 from diagrams.azure.database import *
 from diagrams.azure.web import.*
 
-with Diagram("VG01-MASDCL-A-123608-Dev01-VFSArchiRepo", show=True):
+with Diagram("VG01-MASDCL-A-123608-Dev01-VFSArchiRepo", show=False):
     FunctionApps("Canvas Architecture")
     VMLinux("BackStage App") >> DatabaseForPostgresqlServers("BackStage DB")
     VMWindows("Z123608D01VM1") >> VirtualNetworks("euw-hub03-123608D01-LAZ01-vnet")
